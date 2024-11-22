5b08a95e36d222d1516111b885fa37dd1853c7e5
Giovanna Leão <133283170+giovannaleao10@users.noreply.github.com>
2024-11-22 02:14:29 -0300

    Add files via upload

---
+++ C03EX04O.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+
+    int a, b, c, d, p, s;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+
+    p =  a * c;
+    s = b + d;
+
+    cout << p << endl;
+    cout << s;
+
+    return 0;
+}
