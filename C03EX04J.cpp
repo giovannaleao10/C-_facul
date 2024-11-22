308e68e6106368dbd4cf75560c8f527f03986754
Giovanna Leão <133283170+giovannaleao10@users.noreply.github.com>
2024-11-22 02:13:45 -0300

    Add files via upload

---
+++ C03EX04J.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(void){
+
+    int a, b, r;
+
+    cin >> a;
+    cin >> b;
+
+    r = pow(a - b,2);
+
+    cout << r;
+
+    return 0;
+}
