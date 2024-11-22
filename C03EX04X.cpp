9e267350d94769e46f0fdf79fcb2ac2aed047274
Giovanna Leão <133283170+giovannaleao10@users.noreply.github.com>
2024-11-22 02:15:22 -0300

    Add files via upload

---
+++ C03EX04X.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(void){
+
+    float  b, i, x;
+
+    cin >> b;
+    cin >> i;
+
+    x = pow(b,1/ i);
+
+    cout << x;
+
+    return 0;
+}
